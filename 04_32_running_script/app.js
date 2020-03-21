4d5d48ed90a50cd6d32d4c99c698023fc22fb18a
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-03-21 15:03:40 -0400

    If examples

--- 04_32_running_script/app.js
+++ 04_32_running_script/app.js
@@ -1,1 +1,20 @@
-alert("Hello, world!");
+// alert("Hello, world!");
+
+// conditionals
+
+if (1 === 1) {
+  console.log("It's true");
+}
+
+// Example 2
+let rating = 3;
+if (rating === 3) {
+  console.log("You are a superstar");
+}
+
+// Example 3
+let num = 37;
+
+if (num % 2 !== 0) {
+  console.log("It's odd");
+}
