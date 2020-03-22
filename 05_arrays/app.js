00d713b4195815560ec7e74bd5ca6a0a8f83a56a
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-03-22 12:14:31 -0400

    push pop

--- 05_arrays/app.js
+++ 05_arrays/app.js
@@ -1,2 +1,13 @@
 let shoppingList = ["ice", "cheese", "bread"];
 console.log(shoppingList);
+
+// modify
+shoppingList[1] = "milk";
+
+// add to end
+shoppingList.push("tomatoes");
+console.log(shoppingList);
+
+// remove last
+console.log(shoppingList.pop());
+console.log(shoppingList);
