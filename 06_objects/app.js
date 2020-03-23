26bb460bdf9201f169727c7a25c9249315e1d42e
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-03-23 08:52:03 -0400

    Objects basics

---
+++ 06_objects/app.js
@@ -0,0 +1,10 @@
+const fitbitData = {
+  totalSteps: 100,
+  totalMiles: 12,
+  avgCalorieBurn: 401,
+  workout: "5 of 7",
+  45: "good"
+};
+
+console.log(fitbitData.totalSteps);
+console.log(fitbitData[45]);
