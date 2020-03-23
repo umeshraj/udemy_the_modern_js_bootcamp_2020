5b96f14a5e90e97b6b86ebab8c047350e3e56cf2
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-03-23 08:57:15 -0400

    Access property with []

--- 06_objects/app.js
+++ 06_objects/app.js
@@ -3,8 +3,14 @@ const fitbitData = {
   totalMiles: 12,
   avgCalorieBurn: 401,
   workout: "5 of 7",
-  45: "good"
+  45: "good",
+  "76 game": "fine",
+  red: "#ff0000"
 };
 
 console.log(fitbitData.totalSteps);
 console.log(fitbitData[45]);
+
+// access with []
+console.log(fitbitData["76 game"]);
+console.log(fitbitData["red"]);
