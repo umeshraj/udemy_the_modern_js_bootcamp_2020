fbb631e83ced51e355132c69c46cadf18fd9c46b
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-03-24 08:36:54 -0400

    more for loops

--- 07_loops/app.js
+++ 07_loops/app.js
@@ -2,3 +2,14 @@
 for (let idx = 0; idx < 5; idx++) {
   console.log("Hello");
 }
+
+// for increment
+for (let idx = 0; idx < 5; idx += 4) {
+  console.log("Hello");
+}
+
+// for loop array
+const scores = [30, 2, 132, 12, 102];
+for (let idx = 0; idx < scores.length; idx++) {
+  console.log(`${idx}: ${scores[idx]}`);
+}
