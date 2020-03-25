84f704ddbb0e73b5de08ece85c3fef8610fd5723
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-03-25 08:40:46 -0400

    Password function checker

--- 08_functions/app.js
+++ 08_functions/app.js
@@ -10,3 +10,22 @@ function throwDice(numRolls) {
 }
 
 throwDice(2);
+
+// challenge 1: password validator
+console.log("Password validator");
+function isValidPassword(password, username) {
+  // check length
+  const validLength = password.length >= 8;
+
+  // no spaces
+  const hasNoSpace = password.indexOf(" ") === -1;
+
+  // no username
+  const hasNoUserName = password.indexOf(username) === -1;
+
+  return validLength && hasNoSpace && hasNoUserName;
+}
+
+console.log(isValidPassword("safdsfdafa", "umesh"));
+console.log(isValidPassword(" s asssafasdff", "um"));
+console.log(isValidPassword("asdfdafdfafasfafsa", "asdf"));
