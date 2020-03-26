9b2c4e2f5e07386e253a1a31f903f16bf744b1e4
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-03-26 07:43:30 -0400

    Functions are just values that can be passed

--- 09_functions_advanced/app.js
+++ 09_functions_advanced/app.js
@@ -23,3 +23,32 @@ const square = function(num) {
 
 console.log(square1(5));
 console.log(square(10));
+
+// Higher order functions
+console.log("Higher order functions");
+function add(x, y) {
+  return x + y;
+}
+
+function subtract(x, y) {
+  return x - y;
+}
+
+function multiply(x, y) {
+  return x * y;
+}
+
+function divide(x, y) {
+  return x / y;
+}
+
+const operations = [add, subtract, multiply, divide];
+for (func of operations) {
+  let result = func(10, 5);
+  console.log(result);
+}
+
+const thing = {
+  doSomething: multiply
+};
+console.log(thing.doSomething(50, 2));
