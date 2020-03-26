71bedded567b45334aabeea9ff68fba0e507fb85
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-03-26 07:37:52 -0400

    Function expressions

--- 09_functions_advanced/app.js
+++ 09_functions_advanced/app.js
@@ -9,3 +9,17 @@ function outer() {
 }
 
 outer();
+
+// Function expressions
+console.log("Function expression");
+
+function square1(num) {
+  console.log(num * num);
+}
+
+const square = function(num) {
+  return num * num;
+};
+
+console.log(square1(5));
+console.log(square(10));
