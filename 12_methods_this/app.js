603d76e556bdc73c2cd74096d85d8e52906674b2
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-03-29 15:17:11 -0400

    Computer properties

--- 12_methods_this/app.js
+++ 12_methods_this/app.js
@@ -8,3 +8,22 @@ const getStats = arr => {
 
 const res = getStats([1, 2, 3]);
 console.log(res);
+
+// Computed properties
+const role = "host";
+const person = "Umesh";
+const team = {
+  role: person
+};
+console.log(team);
+
+const team2 = {
+  [role]: person
+};
+console.log(team2);
+
+function addProperty(obj, key, val) {
+  return { ...obj, [key]: val };
+}
+const team3 = addProperty(team, "director", "Mani");
+console.log(team3);
