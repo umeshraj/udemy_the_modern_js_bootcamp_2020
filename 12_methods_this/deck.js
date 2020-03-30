8e949de06e774483755b19645100e6572be8479b
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-03-30 08:39:16 -0400

    Making a deck with functions

---
+++ 12_methods_this/deck.js
@@ -0,0 +1,23 @@
+function makeDeck() {
+  const deck = [];
+  const suits = ["hearts", "diamonds", "spades", "clubs"];
+  const values = "2,3,4,5,6,7,8,9,10,J,Q,K,A";
+
+  for (let value of values.split(",")) {
+    for (let suit of suits) {
+      deck.push({
+        value,
+        suit
+      });
+    }
+  }
+  return deck;
+}
+
+function drawCard(deck) {
+  return deck.pop();
+}
+
+const myDeck = makeDeck();
+const card1 = drawCard(myDeck);
+console.log(card1);
