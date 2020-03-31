ddf8ff82c74535ccd0da0bd79de4859a055a4cbc
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-03-31 07:33:56 -0400

    Cards using class

--- 12_methods_this/deck.js
+++ 12_methods_this/deck.js
@@ -1,23 +1,55 @@
 function makeDeck() {
   const deck = [];
-  const suits = ["hearts", "diamonds", "spades", "clubs"];
-  const values = "2,3,4,5,6,7,8,9,10,J,Q,K,A";
 
-  for (let value of values.split(",")) {
-    for (let suit of suits) {
-      deck.push({
-        value,
-        suit
-      });
-    }
-  }
   return deck;
 }
 
-function drawCard(deck) {
-  return deck.pop();
-}
+// const myDeck = makeDeck();
+// const card1 = drawCard(myDeck);
+// console.log(card1);
 
-const myDeck = makeDeck();
-const card1 = drawCard(myDeck);
-console.log(card1);
+const myDeck = {
+  suits: ["hearts", "diamonds", "spades", "clubs"],
+  values: "2,3,4,5,6,7,8,9,10,J,Q,K,A",
+  deck: [],
+  drawnCards: [],
+  initializeDeck() {
+    const { suits, values, deck } = this;
+    for (let value of values.split(",")) {
+      for (let suit of suits) {
+        deck.push({
+          value,
+          suit
+        });
+      }
+    }
+  },
+
+  drawCard() {
+    const card = this.deck.pop();
+    this.drawnCards.push(card);
+    return card;
+  },
+  drawMultiple(numCards) {
+    const cards = [];
+    for (let i = 0; i < numCards; i++) {
+      cards.push(this.drawCard());
+    }
+    return cards;
+  },
+  shuffle() {
+    const { deck } = this;
+    // loop over array backwards
+    for (let i = deck.length - 1; i > 0; i--) {
+      //pick random index before current element
+      let j = Math.floor(Math.random() * (i + 1));
+      //swap
+      [deck[i], deck[j]] = [deck[j], deck[i]];
+    }
+  }
+};
+
+myDeck.initializeDeck();
+console.log(myDeck.deck);
+const card = myDeck.drawCard();
+console.log(card);
