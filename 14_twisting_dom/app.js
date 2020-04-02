2b951023ce4b9c46825e5ebd14142d898b3d6542
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-04-02 08:32:27 -0400

    Changing multiple elements

--- 14_twisting_dom/app.js
+++ 14_twisting_dom/app.js
@@ -1,3 +1,17 @@
 // Text element
 // const h1 = document.querySelector("h1");
 // console.log(h1.innerText);
+
+// Inner html
+const allLis = document.querySelectorAll("li");
+
+// using old school for loop
+for (let i = 0; i < allLis.length; i++) {
+  //   console.log(allLis[i].innerText);
+  allLis[i].innerText = "We are the best!";
+}
+
+// new for loop
+for (let li of allLis) {
+  li.innerHTML = "We are <b>COOL</b>";
+}
