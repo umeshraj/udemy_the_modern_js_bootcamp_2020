9ce7727179d37e0b612c074f1a2843791141a717
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-04-03 07:38:35 -0400

    Appending various elements

--- 14_twisting_dom/app.js
+++ 14_twisting_dom/app.js
@@ -29,5 +29,29 @@ const todo = document.querySelector("#todos .todo");
 // todo.style.opacity = "50%";
 
 // todo.setAttribute("class", "done");
-todo.classList.add("done");
+// todo.classList.add("done");
 // todo.classList.remove("done");
+
+// Creating elements
+const newh2 = document.createElement("h2");
+newh2.innerText = "I like animals";
+newh2.classList.add("special");
+
+// appending
+const section = document.querySelector("section");
+section.appendChild(newh2);
+
+// adding an image
+const newImg = document.createElement("img");
+newImg.src =
+  "https://images.unsplash.com/photo-1559659133-8781d8f3b673?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9";
+newImg.style.width = "200px";
+document.body.appendChild(newImg);
+
+// adding a youtube link
+const newLink = document.createElement("a");
+newLink.innerText = "Curb Link. Click Me";
+newLink.href = "https://www.youtube.com/watch?v=thZLV6XsrDk";
+
+const firstP = document.querySelector("p");
+firstP.appendChild(newLink);
