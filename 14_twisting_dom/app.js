daed6c69c694d4b891d72b77af284c7489accaa8
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-04-03 07:16:05 -0400

    Making to do list

--- 14_twisting_dom/app.js
+++ 14_twisting_dom/app.js
@@ -3,21 +3,23 @@
 // console.log(h1.innerText);
 
 // Inner html
-const allLis = document.querySelectorAll("li");
+// const allLis = document.querySelectorAll("li");
 
 // using old school for loop
-for (let i = 0; i < allLis.length; i++) {
-  //   console.log(allLis[i].innerText);
-  allLis[i].innerText = "We are the best!";
-}
+// for (let i = 0; i < allLis.length; i++) {
+//   //   console.log(allLis[i].innerText);
+//   allLis[i].innerText = "We are the best!";
+// }
 
 // new for loop
-for (let li of allLis) {
-  li.innerHTML = "We are <b>COOL</b>";
-}
+// for (let li of allLis) {
+//   li.innerHTML = "We are <b>COOL</b>";
+// }
 
-const colors = ["red", "green", "yellow", "orange", "blue", "purple"];
-allLis.forEach((li, i) => {
-  const color = colors[i];
-  li.style.color = color;
-});
+// const colors = ["red", "green", "yellow", "orange", "blue", "purple"];
+// allLis.forEach((li, i) => {
+//   const color = colors[i];
+//   li.style.color = color;
+// });
+
+// TODO list
