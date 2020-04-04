b4ede0c7b121db9350b233899ee9e69d84c0a068
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-04-04 09:35:13 -0400

    Adding a second event to the  button

--- 15_communicate_events/app.js
+++ 15_communicate_events/app.js
@@ -2,3 +2,7 @@ const btn = document.querySelector("#clicker");
 btn.onclick = function() {
   console.log("You clicked the clicker");
 };
+
+btn.ondblclick = function() {
+  console.log("You double clicked");
+};
