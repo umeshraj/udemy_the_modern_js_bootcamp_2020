5fb08ec1305c5b26a53172f897796f9a2e1480e1
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-04-04 07:42:30 -0400

    Printing out teams as a ul

--- 14_nba_chart/app.js
+++ 14_nba_chart/app.js
@@ -1,85 +1,99 @@
-const warriorsGames = [{
+const warriorsGames = [
+  {
     awayTeam: {
-      team: 'Golden State',
+      team: "Golden State",
       points: 119,
       isWinner: true
     },
     homeTeam: {
-      team: 'Houston',
+      team: "Houston",
       points: 106,
       isWinner: false
     }
   },
   {
     awayTeam: {
-      team: 'Golden State',
+      team: "Golden State",
       points: 105,
       isWinner: false
     },
     homeTeam: {
-      team: 'Houston',
+      team: "Houston",
       points: 127,
       isWinner: true
     }
   },
   {
     homeTeam: {
-      team: 'Golden State',
+      team: "Golden State",
       points: 126,
       isWinner: true
     },
     awayTeam: {
-      team: 'Houston',
+      team: "Houston",
       points: 85,
       isWinner: false
     }
   },
   {
     homeTeam: {
-      team: 'Golden State',
+      team: "Golden State",
       points: 92,
       isWinner: false
     },
     awayTeam: {
-      team: 'Houston',
+      team: "Houston",
       points: 95,
       isWinner: true
     }
   },
   {
     awayTeam: {
-      team: 'Golden State',
+      team: "Golden State",
       points: 94,
       isWinner: false
     },
     homeTeam: {
-      team: 'Houston',
+      team: "Houston",
       points: 98,
       isWinner: true
     }
   },
   {
     homeTeam: {
-      team: 'Golden State',
+      team: "Golden State",
       points: 115,
       isWinner: true
     },
     awayTeam: {
-      team: 'Houston',
+      team: "Houston",
       points: 86,
       isWinner: false
     }
   },
   {
     awayTeam: {
-      team: 'Golden State',
+      team: "Golden State",
       points: 101,
       isWinner: true
     },
     homeTeam: {
-      team: 'Houston',
+      team: "Houston",
       points: 92,
       isWinner: false
     }
   }
-]
+];
+
+const ulParent = document.createElement("ul");
+for (game of warriorsGames) {
+  // let awayTeam = game.awayTeam.team;
+  // let homeTeam = game.homeTeam.team;
+  const { homeTeam, awayTeam } = game;
+  const gameLi = document.createElement("li");
+  gameLi.innerText = `${awayTeam.team} @ ${homeTeam.team}`;
+  ulParent.appendChild(gameLi);
+}
+
+// prepend to body so it comes before the script tag
+document.body.prepend(ulParent);
