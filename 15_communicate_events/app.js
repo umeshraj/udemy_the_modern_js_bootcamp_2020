7d05f9e51d235fae0f3c2d0732365795858f68ab
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-04-05 09:28:33 -0400

    Add event listeners

--- 15_communicate_events/app.js
+++ 15_communicate_events/app.js
@@ -1,8 +1,30 @@
-const btn = document.querySelector("#clicker");
-btn.onclick = function() {
-  console.log("You clicked the clicker");
-};
+// const btn = document.querySelector("#clicker");
+// btn.onclick = function() {
+//   console.log("You clicked the clicker");
+// };
 
-btn.ondblclick = function() {
-  console.log("You double clicked");
-};
+// btn.ondblclick = function() {
+//   console.log("You double clicked");
+// };
+
+const btn = document.querySelector("#clicker");
+btn.addEventListener("click", function () {
+  alert("Clicked");
+});
+
+// adding second
+btn.addEventListener("click", function () {
+  console.log("Clicked");
+});
+
+btn.addEventListener("mouseover", function () {
+  btn.innerText = "Stop Touching me";
+});
+
+btn.addEventListener("mouseout", function () {
+  btn.innerText = "Click me";
+});
+
+window.addEventListener("scroll", function () {
+  console.log("Stop scrollin");
+});
