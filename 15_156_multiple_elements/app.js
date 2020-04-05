ca7c510b752ee9c7f79bbdccbb7b7511d907c937
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-04-05 10:21:04 -0400

    Color name picker

---
+++ 15_156_multiple_elements/app.js
@@ -0,0 +1,33 @@
+const colors = [
+  "red",
+  "orange",
+  "yellow",
+  "green",
+  "blue",
+  "purple",
+  "indigo",
+  "violet",
+];
+
+const changeColor = function () {
+  const h1 = document.querySelector("h1");
+  h1.style.color = this.style.backgroundColor;
+  // console.log(this);
+  // console.log(this.style.backgroundColor);
+};
+
+const container = document.querySelector("#boxes");
+for (let color of colors) {
+  const box = document.createElement("div");
+  box.style.background = color;
+  box.classList.add("box");
+
+  box.addEventListener("click", changeColor);
+  // box.addEventListener("click", function () {
+  //   printColor(box);
+  // });
+  // box.addEventListener("click", function () {
+  //   console.log(box.style.backgroundColor);
+  // });
+  container.appendChild(box);
+}
