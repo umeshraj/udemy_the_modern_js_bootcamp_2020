e5e8d538d91ee69038c7c3f4bf13e76d5d71d8e7
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-04-07 08:25:40 -0400

    callback hell with two callbacks

--- 16_166_callback_hell/app.js
+++ 16_166_callback_hell/app.js
@@ -16,28 +16,68 @@ const btn = document.querySelector("button");
 //   }, 1000);
 // }, 1000);
 
-const moveX = (element, amount, delay, callback) => {
+const moveX = (element, amount, delay, onSuccess, onFailure) => {
   const bodyBoundary = document.body.clientWidth;
   const elRight = element.getBoundingClientRect().right;
   const currentLeft = element.getBoundingClientRect().left;
   if (elRight + amount > bodyBoundary) {
-    console.log("Done! CANNOT MOVE THAT FAR");
+    // console.log("Done! CANNOT MOVE THAT FAR");
+    onFailure();
   } else {
     setTimeout(() => {
       element.style.transform = `translateX(${amount + currentLeft}px)`;
-      if (callback) {
-        callback();
-      }
+      //   if (callback) {
+      //     callback();
+      //   }
+      onSuccess();
     }, delay);
   }
 };
 
-moveX(btn, 100, 1000, () => {
-  moveX(btn, 100, 1000, () => {
-    moveX(btn, 100, 1000, () => {
-      moveX(btn, 100, 1000, () => {
-        moveX(btn, 100, 1000);
-      });
-    });
-  });
-});
+// moveX(btn, 100, 1000, () => {
+//   moveX(btn, 100, 1000, () => {
+//     moveX(btn, 100, 1000, () => {
+//       moveX(btn, 100, 1000, () => {
+//         moveX(btn, 100, 1000);
+//       });
+//     });
+//   });
+// });
+
+moveX(
+  btn,
+  100,
+  1000,
+  () => {
+    //success
+    moveX(
+      btn,
+      400,
+      1000,
+      () => {
+        //success
+        moveX(
+          btn,
+          700,
+          1000,
+          () => {
+            //success
+            console.log("Really?!");
+          },
+          () => {
+            //fail
+            alert("Cannot move further");
+          }
+        );
+      },
+      () => {
+        //fail
+        alert("Cannot move further");
+      }
+    );
+  },
+  () => {
+    //failure
+    alert("Cannot move further");
+  }
+);
