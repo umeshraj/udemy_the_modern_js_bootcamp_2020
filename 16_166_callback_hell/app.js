5b1ac520021834503ae204bbca3eb4f029a4bce9
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-04-07 08:09:16 -0400

    Make a function to move

--- 16_166_callback_hell/app.js
+++ 16_166_callback_hell/app.js
@@ -1,17 +1,36 @@
 const btn = document.querySelector("button");
 
-setTimeout(() => {
-  btn.style.transform = `translateX(100px)`;
+// setTimeout(() => {
+//   btn.style.transform = `translateX(100px)`;
+//   setTimeout(() => {
+//     btn.style.transform = `translateX(200px)`;
+//     setTimeout(() => {
+//       btn.style.transform = `translateX(300px)`;
+//       setTimeout(() => {
+//         btn.style.transform = `translateX(400px)`;
+//         setTimeout(() => {
+//           btn.style.transform = `translateX(500px)`;
+//         }, 1000);
+//       }, 1000);
+//     }, 1000);
+//   }, 1000);
+// }, 1000);
+
+const moveX = (element, amount, delay, callback) => {
   setTimeout(() => {
-    btn.style.transform = `translateX(200px)`;
-    setTimeout(() => {
-      btn.style.transform = `translateX(300px)`;
-      setTimeout(() => {
-        btn.style.transform = `translateX(400px)`;
-        setTimeout(() => {
-          btn.style.transform = `translateX(500px)`;
-        }, 1000);
-      }, 1000);
-    }, 1000);
-  }, 1000);
-}, 1000);
+    element.style.transform = `translateX(${amount}px)`;
+    if (callback) {
+      callback();
+    }
+  }, delay);
+};
+
+moveX(btn, 100, 1000, () => {
+  moveX(btn, 200, 1000, () => {
+    moveX(btn, 300, 1000, () => {
+      moveX(btn, 400, 1000, () => {
+        moveX(btn, 500, 1000);
+      });
+    });
+  });
+});
