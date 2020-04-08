aeae935c5696976f7ab2f4e27139f5e9afcb0614
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-04-08 08:12:05 -0400

    Rejecting when url is invalid

--- 16_166_callback_hell/app3.js
+++ 16_166_callback_hell/app3.js
@@ -1,23 +1,22 @@
 const fakeRequest = (url) => {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
-      const rand = Math.random();
-      if (rand < 0.3) {
-        reject({
-          status: 404,
-        });
-      } else {
-        const pages = {
-          "/users": [
-            { id: 1, username: "Bilbo" },
-            { id: 5, username: "Esmerelda" },
-          ],
-          "/about": "This is the about page",
-        };
-        const data = pages[url];
+      const pages = {
+        "/users": [
+          { id: 1, username: "Bilbo" },
+          { id: 5, username: "Esmerelda" },
+        ],
+        "/about": "This is the about page",
+      };
+      const data = pages[url];
+      if (data) {
         resolve({
           status: 200,
           data,
+        });
+      } else {
+        reject({
+          status: 404,
         });
       }
     }, 1000);
@@ -26,11 +25,33 @@ const fakeRequest = (url) => {
 
 fakeRequest("/about")
   .then((res) => {
+    console.log("Request worked");
     console.log(("Status code:", res.status));
     console.log(("Data: ", res.data));
-    console.log("Request worked");
   })
   .catch((res) => {
     console.log(res.status);
     console.log("Failed");
+  });
+
+fakeRequest("/users")
+  .then((res) => {
+    console.log("Request worked");
+    console.log(("Status code:", res.status));
+    console.log(("Data: ", res.data));
+  })
+  .catch((res) => {
+    console.log(res.status);
+    console.log("Failed");
+  });
+
+fakeRequest("/hacked")
+  .then((res) => {
+    console.log("Request worked");
+    console.log(("Status code:", res.status));
+    console.log(("Data: ", res.data));
+  })
+  .catch((res) => {
+    console.log("Request failed");
+    console.log(res.status);
   });
