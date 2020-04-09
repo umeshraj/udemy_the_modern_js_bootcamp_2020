b3278d235389409ecca62efdfa6f983d6b5280f9
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-04-09 07:09:13 -0400

    Modify move  btn with promises

--- 16_166_callback_hell/app.js
+++ 16_166_callback_hell/app.js
@@ -16,23 +16,69 @@ const btn = document.querySelector("button");
 //   }, 1000);
 // }, 1000);
 
-const moveX = (element, amount, delay, onSuccess, onFailure) => {
-  setTimeout(() => {
-    const bodyBoundary = document.body.clientWidth;
-    const elRight = element.getBoundingClientRect().right;
-    const currentLeft = element.getBoundingClientRect().left;
-    if (elRight + amount > bodyBoundary) {
-      // console.log("Done! CANNOT MOVE THAT FAR");
-      onFailure();
-    } else {
-      element.style.transform = `translateX(${amount + currentLeft}px)`;
-      //   if (callback) {
-      //     callback();
-      //   }
-      onSuccess();
-    }
-  }, delay);
+// NEW CODE WITH PROMISE
+const moveX = (element, amount, delay) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const bodyBoundary = document.body.clientWidth;
+      const elRight = element.getBoundingClientRect().right;
+      const currentLeft = element.getBoundingClientRect().left;
+      if (elRight + amount > bodyBoundary) {
+        // console.log("Done! CANNOT MOVE THAT FAR");
+        reject();
+      } else {
+        element.style.transform = `translateX(${amount + currentLeft}px)`;
+        //   if (callback) {
+        //     callback();
+        //   }
+        resolve();
+      }
+    }, delay);
+  });
 };
+
+// arrow function with implicity return
+moveX(btn, 300, 1000)
+  .then(() => moveX(btn, 300, 1000))
+  .then(() => moveX(btn, 300, 1000))
+
+  .catch(() => {
+    console.log("Cannot move");
+  });
+
+// full arrow function
+// moveX(btn, 300, 1000)
+//   .then(() => {
+//     return moveX(btn, 300, 1000);
+//   })
+//   .then(() => {
+//     console.log("Done with both");
+//   })
+//   .then(() => {
+//     console.log("Done with both");
+//   })
+//   .catch(() => {
+//     console.log("Cannot move");
+//   });
+
+// OLD CODE
+// const moveX = (element, amount, delay, onSuccess, onFailure) => {
+//   setTimeout(() => {
+//     const bodyBoundary = document.body.clientWidth;
+//     const elRight = element.getBoundingClientRect().right;
+//     const currentLeft = element.getBoundingClientRect().left;
+//     if (elRight + amount > bodyBoundary) {
+//       // console.log("Done! CANNOT MOVE THAT FAR");
+//       onFailure();
+//     } else {
+//       element.style.transform = `translateX(${amount + currentLeft}px)`;
+//       //   if (callback) {
+//       //     callback();
+//       //   }
+//       onSuccess();
+//     }
+//   }, delay);
+// };
 
 // moveX(btn, 100, 1000, () => {
 //   moveX(btn, 100, 1000, () => {
@@ -44,40 +90,40 @@ const moveX = (element, amount, delay, onSuccess, onFailure) => {
 //   });
 // });
 
-moveX(
-  btn,
-  100,
-  1000,
-  () => {
-    //success
-    moveX(
-      btn,
-      400,
-      1000,
-      () => {
-        //success
-        moveX(
-          btn,
-          700,
-          1000,
-          () => {
-            //success
-            console.log("Really?!");
-          },
-          () => {
-            //fail
-            alert("Cannot move further");
-          }
-        );
-      },
-      () => {
-        //fail
-        alert("Cannot move further");
-      }
-    );
-  },
-  () => {
-    //failure
-    alert("Cannot move further");
-  }
-);
+// moveX(
+//   btn,
+//   100,
+//   1000,
+//   () => {
+//     //success
+//     moveX(
+//       btn,
+//       400,
+//       1000,
+//       () => {
+//         //success
+//         moveX(
+//           btn,
+//           700,
+//           1000,
+//           () => {
+//             //success
+//             console.log("Really?!");
+//           },
+//           () => {
+//             //fail
+//             alert("Cannot move further");
+//           }
+//         );
+//       },
+//       () => {
+//         //fail
+//         alert("Cannot move further");
+//       }
+//     );
+//   },
+//   () => {
+//     //failure
+//     alert("Cannot move further");
+//   }
+// );
