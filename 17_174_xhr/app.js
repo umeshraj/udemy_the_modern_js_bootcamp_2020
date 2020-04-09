13a8118723271844e4a11a0652a62b61f5b9a7d0
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-04-09 08:32:58 -0400

    XHR is not clean if you have to do req based on prev req

--- 17_174_xhr/app.js
+++ 17_174_xhr/app.js
@@ -1,16 +1,33 @@
 const firstReq = new XMLHttpRequest();
+
 firstReq.addEventListener("load", function () {
-  console.log("It worked");
+  console.log("First request worked");
   const data = JSON.parse(this.responseText);
   console.log(data);
-  for (let post of data) {
-    console.log(post.userId);
-  }
+  // for (let post of data) {
+  //   console.log(post.title);
+  // }
+
+  const filmurl = data["species"]["url"];
+  const filmReq = new XMLHttpRequest();
+
+  filmReq.addEventListener("load", function () {
+    console.log("Second request worked");
+    const filmData = JSON.parse(this.responseText);
+    console.log(filmData);
+  });
+  filmReq.addEventListener("error", function (e) {
+    console.log("Trying second request");
+    console.log("ERROR", e);
+  });
+  filmReq.open("GET", filmurl);
+  filmReq.send();
 });
+
 firstReq.addEventListener("error", () => {
   console.log("Error");
 });
 
-firstReq.open("GET", "https://jsonplaceholder.typicode.com/posts");
+firstReq.open("GET", "https://pokeapi.co/api/v2/pokemon/ditto/");
 firstReq.send();
 console.log("Request sent");
