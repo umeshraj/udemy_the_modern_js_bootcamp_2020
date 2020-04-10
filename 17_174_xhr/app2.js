2ef3f68b02579ee71aaf64d62870b41269e9857d
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-04-10 08:40:54 -0400

    Refactoring to adorable flat code

--- 17_174_xhr/app2.js
+++ 17_174_xhr/app2.js
@@ -1,27 +1,34 @@
+const checkStatusAndParse = (response) => {
+  if (!response.ok) {
+    //   console.log("ERROR. 404", response.status);
+    throw new Error(`Status code Error: ${response.status}`);
+  } else {
+    return response.json();
+  }
+};
+
+const printPlanets = (data) => {
+  console.log("Fetched first pokemon");
+  console.log(data);
+
+  //   Clunky way
+  //   const p = new Promise((resolve, reject) => {
+  //     resolve(data);
+  //   });
+  //   return p;
+  const filmurl = data["species"]["url"];
+  return Promise.resolve(filmurl);
+};
+
+const fetchNextUrl = (url) => {
+  return fetch(url);
+};
+
 fetch("https://pokeapi.co/api/v2/pokemon/ditto/")
-  .then((response) => {
-    // console.log(response);
-    if (!response.ok) {
-      //   console.log("ERROR. 404", response.status);
-      throw new Error(`Status code Error: ${response.status}`);
-    } else {
-      return response.json();
-    }
-  })
-  .then((data) => {
-    console.log("Fetched first pokemon");
-    const filmurl = data["species"]["url"];
-    return fetch(filmurl);
-  })
-  .then((response) => {
-    // console.log(response);
-    if (!response.ok) {
-      //   console.log("ERROR. 404", response.status);
-      throw new Error(`Status code Error: ${response.status}`);
-    } else {
-      return response.json();
-    }
-  })
+  .then(checkStatusAndParse)
+  .then(printPlanets)
+  .then(fetchNextUrl)
+  .then(checkStatusAndParse)
   .then((data) => {
     console.log("Fetched first pokemon's info");
     console.log(data);
