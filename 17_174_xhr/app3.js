bd0f904d2eb472e428f5dcf3cf45cc6164094b71
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-04-10 08:52:38 -0400

    Chaining with axios

--- 17_174_xhr/app3.js
+++ 17_174_xhr/app3.js
@@ -1,7 +1,12 @@
 axios
-  .get("https://pokeapi.co/api/v2/pokemon/ditto/dfs")
-  .then((res) => {
-    console.log(res.data);
+  .get("https://pokeapi.co/api/v2/pokemon/ditto/")
+  .then(({ data }) => {
+    console.log(data);
+    const filmurl = data["species"]["url"];
+    return axios.get(filmurl);
+  })
+  .then(({ data }) => {
+    console.log(data);
   })
   .catch((err) => {
     console.log("In my error catcher");
