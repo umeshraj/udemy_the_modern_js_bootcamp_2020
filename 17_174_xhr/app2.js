ed66dec5651cd71708d18ef2a868e3df406dd26e
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-04-10 07:44:32 -0400

    chaining fetch callbacks

--- 17_174_xhr/app2.js
+++ 17_174_xhr/app2.js
@@ -5,8 +5,26 @@ fetch("https://pokeapi.co/api/v2/pokemon/ditto/")
       //   console.log("ERROR. 404", response.status);
       throw new Error(`Status code Error: ${response.status}`);
     } else {
-      response.json().then((data) => console.log(data));
+      return response.json();
     }
+  })
+  .then((data) => {
+    console.log("Fetched first pokemon");
+    const filmurl = data["species"]["url"];
+    return fetch(filmurl);
+  })
+  .then((response) => {
+    // console.log(response);
+    if (!response.ok) {
+      //   console.log("ERROR. 404", response.status);
+      throw new Error(`Status code Error: ${response.status}`);
+    } else {
+      return response.json();
+    }
+  })
+  .then((data) => {
+    console.log("Fetched first pokemon's info");
+    console.log(data);
   })
   .catch((err) => {
     console.log("SOMETHING WENT WRONG", err);
