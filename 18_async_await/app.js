3f68bfaec2311d6fbebe329cb3adc8ba9124f599
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-04-11 09:50:32 -0400

    Rewriting async with promises

--- 18_async_await/app.js
+++ 18_async_await/app.js
@@ -31,3 +31,14 @@ console.log(
       console.log("Promise reject with" + err);
     })
 );
+
+// rewriting with promises
+function add2(x, y) {
+  return new Promise((resolve, reject) => {
+    if (typeof x !== "number" || typeof y !== "number") {
+      reject("x and y must be numbers");
+    } else {
+      resolve(x + y);
+    }
+  });
+}
