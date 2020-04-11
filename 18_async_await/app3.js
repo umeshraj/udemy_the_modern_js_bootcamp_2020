b1b573db8cf598cf8975d000876ad95a448ac1d9
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-04-11 11:06:13 -0400

    Parallel

--- 18_async_await/app3.js
+++ 18_async_await/app3.js
@@ -1,8 +1,21 @@
 // SEQUENTIAL POKEMON ACCESS
+// async function get3Pokemon() {
+//   const poke1 = await axios.get("https://pokeapi.co/api/v2/pokemon/1");
+//   const poke2 = await axios.get("https://pokeapi.co/api/v2/pokemon/2");
+//   const poke3 = await axios.get("https://pokeapi.co/api/v2/pokemon/3");
+//   console.log(poke1.data);
+//   console.log(poke2.data);
+//   console.log(poke3.data);
+// }
+
+// PARALLEL
 async function get3Pokemon() {
-  const poke1 = await axios.get("https://pokeapi.co/api/v2/pokemon/1");
-  const poke2 = await axios.get("https://pokeapi.co/api/v2/pokemon/2");
-  const poke3 = await axios.get("https://pokeapi.co/api/v2/pokemon/3");
+  const prom1 = axios.get("https://pokeapi.co/api/v2/pokemon/1");
+  const prom2 = axios.get("https://pokeapi.co/api/v2/pokemon/2");
+  const prom3 = axios.get("https://pokeapi.co/api/v2/pokemon/3");
+  const poke1 = await prom1;
+  const poke2 = await prom2;
+  const poke3 = await prom3;
   console.log(poke1.data);
   console.log(poke2.data);
   console.log(poke3.data);
