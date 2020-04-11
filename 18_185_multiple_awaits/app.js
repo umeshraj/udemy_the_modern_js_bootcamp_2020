b8c081732055813816531195a9d83b1c16c52d6a
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-04-11 10:53:07 -0400

    Modifying old promise code to use async

--- 18_185_multiple_awaits/app.js
+++ 18_185_multiple_awaits/app.js
@@ -37,15 +37,29 @@ const moveX = (element, amount, delay) => {
   });
 };
 
-// arrow function with implicity return
-moveX(btn, 300, 1000)
-  .then(() => moveX(btn, 300, 1000))
-  .then(() => moveX(btn, 300, 1000))
-  .then(() => moveX(btn, 300, 1000))
-  .catch(({ bodyBoundary, elRight, amount }) => {
-    console.log(`Body is ${bodyBoundary}px`);
-    console.log(`Element is at ${elRight}px,${amount}px is too large`);
-  });
+async function animateRight(el, amt) {
+  await moveX(el, amt, 1000);
+  await moveX(el, amt, 1000);
+  await moveX(el, amt, 1000);
+  await moveX(el, amt, 1000);
+  await moveX(el, amt, 1000);
+  await moveX(el, amt, 1000);
+}
+
+animateRight(btn, 100).catch((err) => {
+  console.log("All done!");
+  animateRight(btn, -100);
+});
+
+// // arrow function with implicity return
+// moveX(btn, 300, 1000)
+//   .then(() => moveX(btn, 300, 1000))
+//   .then(() => moveX(btn, 300, 1000))
+//   .then(() => moveX(btn, 300, 1000))
+//   .catch(({ bodyBoundary, elRight, amount }) => {
+//     console.log(`Body is ${bodyBoundary}px`);
+//     console.log(`Element is at ${elRight}px,${amount}px is too large`);
+//   });
 
 // full arrow function
 // moveX(btn, 300, 1000)
