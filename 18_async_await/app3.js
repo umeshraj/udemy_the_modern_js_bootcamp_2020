cbeaf689cf85fa28f8ca92835d7987229a2b738e
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-04-11 11:14:49 -0400

    Sequential vs parallel

--- 18_async_await/app3.js
+++ 18_async_await/app3.js
@@ -9,16 +9,45 @@
 // }
 
 // PARALLEL
-async function get3Pokemon() {
-  const prom1 = axios.get("https://pokeapi.co/api/v2/pokemon/1");
-  const prom2 = axios.get("https://pokeapi.co/api/v2/pokemon/2");
-  const prom3 = axios.get("https://pokeapi.co/api/v2/pokemon/3");
-  const poke1 = await prom1;
-  const poke2 = await prom2;
-  const poke3 = await prom3;
-  console.log(poke1.data);
-  console.log(poke2.data);
-  console.log(poke3.data);
+// async function get3Pokemon() {
+//   const prom1 = axios.get("https://pokeapi.co/api/v2/pokemon/1");
+//   const prom2 = axios.get("https://pokeapi.co/api/v2/pokemon/2");
+//   const prom3 = axios.get("https://pokeapi.co/api/v2/pokemon/3");
+//   const poke1 = await prom1;
+//   const poke2 = await prom2;
+//   const poke3 = await prom3;
+//   console.log(poke1.data);
+//   console.log(poke2.data);
+//   console.log(poke3.data);
+// }
+
+// get3Pokemon();
+
+function changeBodyColor(color, delay) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      document.body.style.backgroundColor = color;
+      resolve();
+    }, delay);
+  });
 }
 
-get3Pokemon();
+// sequential
+// async function lightShow() {
+//   await changeBodyColor("teal", 1000);
+//   await changeBodyColor("pink", 1000);
+//   await changeBodyColor("indigo", 1000);
+//   await changeBodyColor("violet", 1000);
+// }
+
+async function lightShow() {
+  const prom1 = changeBodyColor("teal", 1000);
+  const prom2 = changeBodyColor("pink", 1000);
+  const prom3 = changeBodyColor("indigo", 1000);
+  const prom4 = changeBodyColor("violet", 1000);
+  await prom1;
+  await prom2;
+  await prom3;
+  await prom4;
+}
+lightShow();
