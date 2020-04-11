389f72fc6ae0046aa86044d1b4c824cd71102e3b
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-04-11 09:58:02 -0400

    await usage

--- 18_async_await/app2.js
+++ 18_async_await/app2.js
@@ -1,7 +1,14 @@
-function getData() {
-  return axios.get("https://jsonplaceholder.typicode.com/todos/1");
-}
+// function getData() {
+//   return axios.get("https://jsonplaceholder.typicode.com/todos/1");
+// }
 
-getData().then((res) => {
+// getData().then((res) => {
+//   console.log(res.data);
+// });
+
+// new way with async/await
+async function getData() {
+  const res = await axios.get("https://jsonplaceholder.typicode.com/todos/1");
   console.log(res.data);
-});
+}
+getData();
