54eaecfe7f1fd57f16997cafc00816ea5054d0d5
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-04-12 11:11:14 -0400

    the new keyword with function makes a class

--- 19_oop/app_190.js
+++ 19_oop/app_190.js
@@ -9,7 +9,7 @@ function rgb(r, g, b) {
 console.log(hex(255, 100, 25));
 console.log(rgb(255, 0, 12));
 
-// Factor function
+// Factory function
 function makeColor(r, g, b) {
   const color = {}; // function makes a new object
   color.r = r;
@@ -26,6 +26,38 @@ function makeColor(r, g, b) {
 
   return color; // function returns the object it created
 }
-
 firstColor = makeColor(35, 255, 150);
 firstColor.rgb();
+
+// Making constructors
+console.log("Constructors!!");
+function Color(r, g, b) {
+  this.r = r;
+  this.g = g;
+  this.b = b;
+  //   console.log(this);
+}
+
+// add a function to the prototype
+Color.prototype.rgb = function () {
+  const { r, g, b } = this;
+  return `rgb(${r}, ${this.g}, ${b})`;
+};
+
+Color.prototype.hex = function hex() {
+  const { r, g, b } = this;
+  return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+};
+
+Color.prototype.rgba = function (a = 1) {
+  const { r, g, b } = this;
+  return `rgba(${r}, ${g}, ${b}, ${a})`;
+};
+
+const color1 = new Color(123, 90, 12);
+const color2 = new Color(12, 35, 46);
+console.log(color1.hex());
+console.log(color2.hex());
+console.log(color1.hex == color2.hex);
+
+console.log(color1.rgba(0.5));
