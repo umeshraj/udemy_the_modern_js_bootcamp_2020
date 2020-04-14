3c2b9862741b43775ccb5e0c0981138c0035595f
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-04-14 07:23:33 -0400

    My attempt using input/h1 and buttons

--- 20_drawing_animations_timer/index.js
+++ 20_drawing_animations_timer/index.js
@@ -1,1 +1,20 @@
-console.log("Hi there");
+btnStart = document.querySelector("#btn-start");
+btnStop = document.querySelector("#btn-stop");
+timerTxt = document.querySelector("#timer");
+let interval;
+
+btnStart.addEventListener("click", () => {
+  interval = setInterval(() => {
+    // timerVal = parseInt(timerTxt.innerText);
+    timerVal = parseInt(timerTxt.value);
+
+    if (timerVal > 0) {
+      //   timerTxt.innerText = timerVal - 1;
+      timerTxt.value = timerVal - 1;
+    }
+  }, 1000);
+});
+
+btnStop.addEventListener("click", () => {
+  clearInterval(interval);
+});
