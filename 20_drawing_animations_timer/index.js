1a2e210c1d62a26fcb70495a87b523e48deb7a2d
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-04-15 08:16:27 -0400

    Adding onTick, onComplete

--- 20_drawing_animations_timer/index.js
+++ 20_drawing_animations_timer/index.js
@@ -5,6 +5,8 @@ class Timer {
     this.pauseButton = pauseButton;
     if (callbacks) {
       this.onStart = callbacks.onStart;
+      this.onTick = callbacks.onTick;
+      this.onComplete = callbacks.onComplete;
     }
 
     // option a
@@ -35,8 +37,14 @@ class Timer {
   tick = () => {
     if (this.timeRemaining <= 0) {
       this.pause();
+      if (this.onComplete) {
+        this.onComplete();
+      }
     } else {
       this.timeRemaining = this.timeRemaining - 1;
+      if (this.onTick) {
+        this.onTick();
+      }
     }
   };
 
@@ -58,7 +66,11 @@ const timer = new Timer(durationInput, startButton, pauseButton, {
   onStart() {
     console.log("Timer started");
   },
-  onTick() {},
-  onComplete() {},
+  onTick() {
+    console.log("Timer ticked");
+  },
+  onComplete() {
+    console.log("Timer is done");
+  },
 });
 // timer.start();
