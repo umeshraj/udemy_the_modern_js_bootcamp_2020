4e718671bc39a87e9128ed6402c62b423a62985b
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-04-15 08:13:21 -0400

    Connecting Timer to external world

--- 20_drawing_animations_timer/index.js
+++ 20_drawing_animations_timer/index.js
@@ -1,8 +1,11 @@
 class Timer {
-  constructor(durationInput, startButton, pauseButton) {
+  constructor(durationInput, startButton, pauseButton, callbacks) {
     this.durationInput = durationInput;
     this.startButton = startButton;
     this.pauseButton = pauseButton;
+    if (callbacks) {
+      this.onStart = callbacks.onStart;
+    }
 
     // option a
     this.startButton.addEventListener("click", this.start);
@@ -13,6 +16,9 @@ class Timer {
 
   //   option A
   start = () => {
+    if (this.onStart) {
+      this.onStart();
+    }
     this.tick(); // to start immediately when start is pressed
     this.interval = setInterval(this.tick, 1000);
   };
@@ -27,7 +33,11 @@ class Timer {
   };
 
   tick = () => {
-    this.timeRemaining = this.timeRemaining - 1;
+    if (this.timeRemaining <= 0) {
+      this.pause();
+    } else {
+      this.timeRemaining = this.timeRemaining - 1;
+    }
   };
 
   get timeRemaining() {
@@ -44,5 +54,11 @@ const durationInput = document.querySelector("#duration");
 const startButton = document.querySelector("#start");
 const pauseButton = document.querySelector("#pause");
 
-const timer = new Timer(durationInput, startButton, pauseButton);
+const timer = new Timer(durationInput, startButton, pauseButton, {
+  onStart() {
+    console.log("Timer started");
+  },
+  onTick() {},
+  onComplete() {},
+});
 // timer.start();
