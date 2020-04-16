7ac534447217cfb121907f4f0f4ef7335b52ee72
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-04-16 07:38:30 -0400

    First step in animating circle

--- 20_drawing_animations_timer/index.js
+++ 20_drawing_animations_timer/index.js
@@ -2,13 +2,21 @@
 const durationInput = document.querySelector("#duration");
 const startButton = document.querySelector("#start");
 const pauseButton = document.querySelector("#pause");
+const circle = document.querySelector("circle");
+
+const perimeter = parseFloat(circle.getAttribute("r")) * 2 * Math.PI;
+circle.setAttribute("stroke-dasharray", perimeter);
+
+let currentOffset = 0;
 
 const timer = new Timer(durationInput, startButton, pauseButton, {
   onStart() {
+    // circle.setAttribute('stroke-dashoffset')
     console.log("Timer started");
   },
   onTick() {
-    console.log("Timer ticked");
+    circle.setAttribute("stroke-dashoffset", currentOffset);
+    currentOffset -= 10;
   },
   onComplete() {
     console.log("Timer is done");
