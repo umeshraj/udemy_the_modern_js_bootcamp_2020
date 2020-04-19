bf40c7f9168b7f83c42b56ab45198c5378c62e1c
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-04-19 10:38:37 -0400

    Get data for clicked movie

--- 21_movie_search/index.js
+++ 21_movie_search/index.js
@@ -56,6 +56,7 @@ const onInput = async (event) => {
     option.addEventListener("click", () => {
       dropdown.classList.remove("is-active");
       input.value = movie.Title;
+      onMovieSelect(movie);
     });
     resultsWrapper.appendChild(option);
   }
@@ -68,3 +69,14 @@ document.addEventListener("click", (event) => {
     dropdown.classList.remove("is-active");
   }
 });
+
+// on movie select
+const onMovieSelect = async (movie) => {
+  const response = await axios.get("http://www.omdbapi.com/", {
+    params: {
+      apikey: apikey,
+      i: movie.imdbID,
+    },
+  });
+  console.log(response.data);
+};
