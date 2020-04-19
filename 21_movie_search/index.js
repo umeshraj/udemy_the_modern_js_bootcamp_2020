46d56625d73a2360eb250c3e7bae271f10af0efb
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-04-19 11:32:10 -0400

    Reusable autocomplete

--- 21_movie_search/index.js
+++ 21_movie_search/index.js
@@ -14,62 +14,6 @@ const fetchData = async (searchTerm) => {
   return response.data.Search;
 };
 
-const root = document.querySelector(".autocomplete");
-root.innerHTML = `
-  <label><b>Search for a movie</b></label>
-  <input class="input" />
-  <div class="dropdown">
-    <div class="dropdown-menu">
-      <div class="dropdown-content results">
-      </div>
-    </div>
-  </div>
-`;
-const input = document.querySelector("input");
-const dropdown = document.querySelector(".dropdown");
-const resultsWrapper = document.querySelector(".results");
-
-const onInput = async (event) => {
-  const movies = await fetchData(event.target.value);
-  // console.log(movies);
-
-  // close dropdown if no results
-  if (!movies.length) {
-    dropdown.classList.remove("is-active");
-    return;
-  }
-  // clear existing dropdown
-  resultsWrapper.innerHTML = "";
-  dropdown.classList.add("is-active");
-  for (let movie of movies) {
-    const option = document.createElement("a");
-    // check if image src is n/a
-    const imgSrc = movie.Poster === "N/A" ? "" : movie.Poster;
-
-    option.classList.add("dropdown-item");
-    option.innerHTML = `
-      <img src="${imgSrc}"/>
-      ${movie.Title}
-    `;
-
-    // handle click on option
-    option.addEventListener("click", () => {
-      dropdown.classList.remove("is-active");
-      input.value = movie.Title;
-      onMovieSelect(movie);
-    });
-    resultsWrapper.appendChild(option);
-  }
-};
-input.addEventListener("input", debounce(onInput, 1000));
-
-// close the dropdown
-document.addEventListener("click", (event) => {
-  if (!root.contains(event.target)) {
-    dropdown.classList.remove("is-active");
-  }
-});
-
 // on movie select
 const onMovieSelect = async (movie) => {
   const response = await axios.get("http://www.omdbapi.com/", {
@@ -81,6 +25,19 @@ const onMovieSelect = async (movie) => {
   // console.log(response.data);
   document.querySelector("#summary").innerHTML = movieTemplate(response.data);
 };
+
+// create first search
+createAutoComplete({
+  root: document.querySelector(".autocomplete"),
+});
+// create second search
+createAutoComplete({
+  root: document.querySelector(".autocomplete-two"),
+});
+// create third search
+createAutoComplete({
+  root: document.querySelector(".autocomplete-three"),
+});
 
 const movieTemplate = (movieDetail) => {
   return `
