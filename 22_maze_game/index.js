78e2d5f02b861795e5625fee2835613ac6998056
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-04-21 08:51:10 -0400

    Adding several shapes

--- 22_maze_game/index.js
+++ 22_maze_game/index.js
@@ -1,4 +1,15 @@
-const { Engine, Render, Runner, World, Bodies } = Matter;
+const {
+  Engine,
+  Render,
+  Runner,
+  World,
+  Bodies,
+  MouseConstraint,
+  Mouse,
+} = Matter;
+
+const width = 800;
+const height = 600;
 
 const engine = Engine.create();
 const { world } = engine;
@@ -6,12 +17,21 @@ const render = Render.create({
   element: document.body,
   engine: engine,
   options: {
-    width: 800,
-    height: 600,
+    wireframes: false,
+    width,
+    height,
   },
 });
 Render.run(render);
 Runner.run(Runner.create(), engine);
+
+// adding mouse
+World.add(
+  world,
+  MouseConstraint.create(engine, {
+    mouse: Mouse.create(render.canvas),
+  })
+);
 
 // Walls
 const walls = [
@@ -20,7 +40,23 @@ const walls = [
   Bodies.rectangle(0, 300, 40, 600, { isStatic: true }),
   Bodies.rectangle(800, 300, 40, 600, { isStatic: true }),
 ];
-
 World.add(world, walls);
 
-World.add(world, Bodies.rectangle(200, 200, 50, 50));
+// random shapes
+for (let i = 0; i < 40; i++) {
+  if (Math.random() > 0.5) {
+    World.add(
+      world,
+      Bodies.rectangle(Math.random() * width, Math.random() * height, 50, 50)
+    );
+  } else {
+    World.add(
+      world,
+      Bodies.circle(Math.random() * width, Math.random() * height, 35, {
+        render: {
+          fillStyle: "red",
+        },
+      })
+    );
+  }
+}
