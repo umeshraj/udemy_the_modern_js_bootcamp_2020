3696658f07a4153d86650c472ac65f80f877a0e2
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-04-26 09:14:18 -0400

    First implementation of folder listing

--- 24_01_dirlist/index.js
+++ 24_01_dirlist/index.js
@@ -1,1 +1,11 @@
 const fs = require("fs");
+
+fs.readdir(".", (err, filenames) => {
+  // err is null or an object
+  if (err) {
+    // error handling
+    console.log(err);
+    return;
+  }
+  console.log(filenames);
+});
