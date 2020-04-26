46a2cd5830458d22de30b1f87459aef07f64a050
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-04-26 10:08:20 -0400

    Using lstat to get file/folder info; but o/p order not fixed

--- 24_01_dirlist/index.js
+++ 24_01_dirlist/index.js
@@ -7,5 +7,16 @@ fs.readdir(process.cwd(), (err, filenames) => {
     console.log(err);
     return;
   }
-  console.log(filenames);
+  //   console.log(filenames);
+
+  // bad code
+  for (let filename of filenames) {
+    fs.lstat(filename, (err, stats) => {
+      if (err) {
+        console.log(err);
+      }
+      console.log(filename, stats.isFile());
+    });
+  }
+  // bad code ends
 });
