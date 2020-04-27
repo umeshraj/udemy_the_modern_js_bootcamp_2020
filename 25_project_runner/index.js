8947907d8807cd4fd7319e92febbd69fc558368a
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-04-27 08:35:55 -0400

    Testing chokidar

--- 25_project_runner/index.js
+++ 25_project_runner/index.js
@@ -1,3 +1,15 @@
 #!usr/bin/env node
 
-console.log("I was executed");
+chokidar = require("chokidar");
+
+chokidar
+  .watch(".")
+  .on("add", () => {
+    console.log("File added");
+  })
+  .on("change", () => {
+    console.log("File changed");
+  })
+  .on("unlink", () => {
+    console.log("File unliked");
+  });
