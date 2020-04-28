f8a003fb30cfbd33c6f397935887d7123b96c67c
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-04-28 08:44:29 -0400

    Setting up express to listen & respond to /

--- 26_ecommerce/index.js
+++ 26_ecommerce/index.js
@@ -1,1 +1,12 @@
-console.log("Hi, there!");
+const express = require("express");
+
+const app = express();
+
+// route handler
+app.get("/", (req, res) => {
+  res.send("Hi there!");
+});
+
+app.listen(3000, () => {
+  console.log("Listening");
+});
