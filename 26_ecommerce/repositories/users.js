cba555da8144505ce517076f6a9e15087ed97e58
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-04-29 07:52:09 -0400

    Creating a users.json to store user info

---
+++ 26_ecommerce/repositories/users.js
@@ -0,0 +1,20 @@
+const fs = require("fs");
+
+class UserRepository {
+  constructor(filename) {
+    if (!filename) {
+      throw new Error("Creating a repo needs a filename");
+    }
+
+    this.filename = filename;
+    // check if file exists
+    try {
+      fs.accessSync(this.filename);
+    } catch {
+      fs.writeFileSync(this.filename, "[]");
+    }
+  }
+}
+
+// testing
+new UserRepository("users.json");
