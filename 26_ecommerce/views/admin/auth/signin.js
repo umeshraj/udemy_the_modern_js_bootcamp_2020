a817e0529339874b07c2d0928049a3e1a9635959
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-05-02 09:12:10 -0400

    Using layouts

--- 26_ecommerce/views/admin/auth/signin.js
+++ 26_ecommerce/views/admin/auth/signin.js
@@ -1,11 +1,15 @@
+const layout = require("../layout");
+
 module.exports = () => {
-  return `
-    <div>
-      <form method="POST">
-        <input type="text" name="email" placeholder="email" />
-        <input type="text" name="password" placeholder="password" />
-        <button>Sign in</button>
-      </form>
-    </div>
-    `;
+  return layout({
+    content: `
+  <div>
+    <form method="POST">
+      <input type="text" name="email" placeholder="email" />
+      <input type="text" name="password" placeholder="password" />
+      <button>Sign in</button>
+    </form>
+  </div>
+  `,
+  });
 };
--- 26_ecommerce/views/admin/auth/signup.js
+++ 26_ecommerce/views/admin/auth/signup.js
@@ -1,13 +1,17 @@
+const layout = require("../layout");
+
 module.exports = ({ req }) => {
-  return `
-    <div>
-    Your id is: ${req.session.userId}
-    <form method="POST">
-      <input type="text" name="email" placeholder="email" />
-      <input type="text" name="password" placeholder="password" />
-      <input type="text" name="passwordConfirmation" placeholder="password confirm" />
-      <button>Sign up</button>
-    </form>
-  </div>
-  `;
+  return layout({
+    content: `
+      <div>
+      Your id is: ${req.session.userId}
+      <form method="POST">
+        <input type="text" name="email" placeholder="email" />
+        <input type="text" name="password" placeholder="password" />
+        <input type="text" name="passwordConfirmation" placeholder="password confirm" />
+        <button>Sign up</button>
+      </form>
+    </div>
+    `,
+  });
 };
