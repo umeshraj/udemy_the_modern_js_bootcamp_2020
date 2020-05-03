9809c20ea2a4ab08c18ec5544ae8573a24eb6752
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-05-03 10:08:02 -0400

    tab indent

--- 26_ecommerce/views/admin/auth/signin.js
+++ 26_ecommerce/views/admin/auth/signin.js
@@ -3,13 +3,13 @@ const layout = require("../layout");
 module.exports = () => {
   return layout({
     content: `
-  <div>
-    <form method="POST">
-      <input type="text" name="email" placeholder="email" />
-      <input type="text" name="password" placeholder="password" />
-      <button>Sign in</button>
-    </form>
-  </div>
+      <div>
+        <form method="POST">
+          <input type="text" name="email" placeholder="email" />
+          <input type="text" name="password" placeholder="password" />
+          <button>Sign in</button>
+        </form>
+      </div>
   `,
   });
 };
