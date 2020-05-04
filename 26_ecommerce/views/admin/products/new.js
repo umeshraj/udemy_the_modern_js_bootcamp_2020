724fb4f8455d9969998e720766f612b7eb516247
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-05-04 08:06:10 -0400

    Creating new product page

---
+++ 26_ecommerce/views/admin/products/new.js
@@ -0,0 +1,16 @@
+const layout = require("../layout");
+const { getError } = require("../../helpers");
+
+module.exports = ({ errors }) => {
+  return layout({
+    content: `
+    <form method="POST">
+    <input type="text" placeholder="Title" name="title" />
+    <input type="text" placeholder="Price" name="price" />
+    <input type="file" name="image" id="" />
+    <button>Submit</button>
+  </form>
+  
+    `,
+  });
+};
