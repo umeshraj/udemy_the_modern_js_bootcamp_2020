dc5162cfcbe4b3003d059844a2aea8bd2c90b147
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-05-04 06:53:05 -0400

    Move common function to a file

--- 26_ecommerce/routes/admin/auth.js
+++ 26_ecommerce/routes/admin/auth.js
@@ -48,7 +48,7 @@ router.get("/signout", (req, res) => {
 });
 
 router.get("/signin", (req, res) => {
-  res.send(signinTemplate());
+  res.send(signinTemplate({}));
 });
 
 router.post(
@@ -56,7 +56,9 @@ router.post(
   [requireEmailExists, requireValidPasswordForUser],
   async (req, res) => {
     const errors = validationResult(req);
-    console.log(errors);
+    if (!errors.isEmpty()) {
+      return res.send(signinTemplate({ errors }));
+    }
     const { email } = req.body;
     const user = await usersRepo.getOneBy({ email });
 
