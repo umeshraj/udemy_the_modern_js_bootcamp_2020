932c1c841ef994ee70c599462ad6d9c4db8c848e
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-05-05 08:06:58 -0400

    Handle errors & Swap middleware order

--- 26_ecommerce/views/admin/products/new.js
+++ 26_ecommerce/views/admin/products/new.js
@@ -6,7 +6,9 @@ module.exports = ({ errors }) => {
     content: `
     <form method="POST" enctype="multipart/form-data">
     <input type="text" placeholder="Title" name="title" />
+    ${getError(errors, "title")}
     <input type="text" placeholder="Price" name="price" />
+    ${getError(errors, "price")}
     <input type="file" name="image" id="" />
     <button>Submit</button>
   </form>
