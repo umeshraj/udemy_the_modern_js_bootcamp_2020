c5dd93e22a6b565606f2c92055cde8b2aa8d8038
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-05-05 07:14:30 -0400

    Add multer middleware

--- 26_ecommerce/routes/admin/products.js
+++ 26_ecommerce/routes/admin/products.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const { validationResult } = require("express-validator");
+const multer = require("multer");
+
 const productsRepo = require("../../repositories/products");
 const productsNewTemplate = require("../../views/admin/products/new");
 const { requireTitle, requirePrice } = require("./validators");
 
 const router = express.Router();
+const upload = multer({ storage: multer.memoryStorage() });
 
 router.get("/admin/products", (req, res) => {});
 
@@ -12,15 +15,19 @@ router.get("/admin/products/new", (req, res) => {
   res.send(productsNewTemplate({}));
 });
 
-router.post("/admin/products/new", [requireTitle, requirePrice], (req, res) => {
-  const errors = validationResult(req);
+router.post(
+  "/admin/products/new",
+  [requireTitle, requirePrice],
+  upload.single("image"),
+  (req, res) => {
+    const errors = validationResult(req);
+    console.log(req.file);
+    // req.on("data", (data) => {
+    //   console.log(data.toString());
+    // });
 
-  console.log("Hello!");
-  req.on("data", (data) => {
-    console.log(data.toString());
-  });
-
-  res.send("Submitted");
-});
+    res.send("Submitted");
+  }
+);
 
 module.exports = router;
