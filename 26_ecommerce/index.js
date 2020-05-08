4016c222bc0db38ca6c1562e8238462147d8d8c9
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-05-08 07:08:10 -0400

    Setting up cart page

--- 26_ecommerce/index.js
+++ 26_ecommerce/index.js
@@ -4,6 +4,7 @@ const cookieSession = require("cookie-session");
 const authRouter = require("./routes/admin/auth");
 const adminProductsRouter = require("./routes/admin/products");
 const productsRouter = require("./routes/products");
+const cartsRouter = require("./routes/carts");
 
 const app = express();
 
@@ -13,6 +14,7 @@ app.use(cookieSession({ keys: ["safkj2343sfsssssf"] }));
 app.use(authRouter);
 app.use(productsRouter);
 app.use(adminProductsRouter);
+app.use(cartsRouter);
 
 app.listen(3000, () => {
   console.log("Listening");
---
+++ 26_ecommerce/routes/carts.js
@@ -0,0 +1,17 @@
+const express = require("express");
+// const productRepo = require("../repositories/products");
+// const productsIndexTemplate = require("../views/products/index");
+
+const router = express.Router();
+
+// receive a post request to add item to cart
+
+// receive a get request to show all items in cart
+
+// receive a post request to delete item from cart
+// router.get("/", async (req, res) => {
+//   const products = await productRepo.getAll();
+//   res.send(productsIndexTemplate({ products }));
+// });
+
+module.exports = router;
