2874eb58f1028d2bb852f64f4b19aeeba5610e71
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-05-08 07:48:26 -0400

    Routes to handle adding to cart

--- 26_ecommerce/routes/carts.js
+++ 26_ecommerce/routes/carts.js
@@ -5,13 +5,13 @@ const express = require("express");
 const router = express.Router();
 
 // receive a post request to add item to cart
+router.post("/cart/products", (req, res) => {
+  console.log(req.body.productId);
+  res.send("Product added to cart!");
+});
 
 // receive a get request to show all items in cart
 
 // receive a post request to delete item from cart
-// router.get("/", async (req, res) => {
-//   const products = await productRepo.getAll();
-//   res.send(productsIndexTemplate({ products }));
-// });
 
 module.exports = router;
