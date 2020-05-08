8a1e79031689acbd5311d5f223d889d5fb56340e
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-05-08 06:50:32 -0400

    Showing products list

--- 26_ecommerce/routes/products.js
+++ 26_ecommerce/routes/products.js
@@ -1,9 +1,12 @@
 const express = require("express");
+const productRepo = require("../repositories/products");
+const productsIndexTemplate = require("../views/products/index");
 
 const router = express.Router();
 
 router.get("/", async (req, res) => {
-  res.send("Products!!");
+  const products = await productRepo.getAll();
+  res.send(productsIndexTemplate({ products }));
 });
 
 module.exports = router;
