288090b48f7f8223edfddb50af18c299b52ffe5e
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-05-08 08:11:30 -0400

    Create or adding to existing cart

--- 26_ecommerce/routes/carts.js
+++ 26_ecommerce/routes/carts.js
@@ -1,12 +1,27 @@
 const express = require("express");
-// const productRepo = require("../repositories/products");
+const cartsRepo = require("../repositories/carts");
 // const productsIndexTemplate = require("../views/products/index");
 
 const router = express.Router();
 
 // receive a post request to add item to cart
-router.post("/cart/products", (req, res) => {
-  console.log(req.body.productId);
+router.post("/cart/products", async (req, res) => {
+  //figure out the cart
+  let cart;
+  if (!req.session.cartId) {
+    // we don't have a cart; need to create one and store the cart id on req.session cartId property
+    cart = await cartsRepo.create({ items: [] });
+    req.session.cartId = cart.id;
+  } else {
+    //   we have a cart. Get it from repo
+    console.log(req.session.cartId);
+    cart = await cartsRepo.getOne(req.session.cartId);
+  }
+
+  console.log(cart);
+
+  // either increment quantity or add new product
+
   res.send("Product added to cart!");
 });
 
