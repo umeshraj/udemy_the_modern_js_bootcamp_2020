43f8e3196d3d94c9c103b4d5d2bf810fc0a8bf4c
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-05-09 08:48:34 -0400

    Adding to cart

--- 26_ecommerce/routes/carts.js
+++ 26_ecommerce/routes/carts.js
@@ -18,9 +18,21 @@ router.post("/cart/products", async (req, res) => {
     cart = await cartsRepo.getOne(req.session.cartId);
   }
 
-  console.log(cart);
-
   // either increment quantity or add new product
+  const existingItem = cart.items.find(
+    (item) => item.id === req.body.productId
+  );
+  console.log(existingItem);
+  if (existingItem) {
+    // increment quantity
+    existingItem.quantity++;
+  } else {
+    // add new productId
+    cart.items.push({ id: req.body.productId, quantity: 1 });
+  }
+  await cartsRepo.update(cart.id, {
+    items: cart.items,
+  });
 
   res.send("Product added to cart!");
 });
