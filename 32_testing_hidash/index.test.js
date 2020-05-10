dad0b2e3e20751ade3de20cdb03f09f2cc64908b
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-05-10 09:54:29 -0400

    assert with deepStrictEqual

--- 32_testing_hidash/index.test.js
+++ 32_testing_hidash/index.test.js
@@ -24,7 +24,7 @@ test("The map function", () => {
     return value * 2;
   });
 
-  assert.deepEqual(result, [2, 4, 6]);
+  assert.deepStrictEqual(result, [2, 4, 7]);
   //   assert.strictEqual(result[0], 2);
   //   assert.strictEqual(result[1], 4);
   //   assert.strictEqual(result[2], 6);
