be9468023de9dba4c31a0300de1b12b2d9039ad3
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-05-10 09:07:53 -0400

    Map function with test

--- 32_testing_hidash/index.test.js
+++ 32_testing_hidash/index.test.js
@@ -1,4 +1,4 @@
-const { forEach } = require("./index");
+const { forEach, map } = require("./index");
 
 let sum = 0;
 forEach([1, 2, 3], (value) => {
@@ -7,4 +7,18 @@ forEach([1, 2, 3], (value) => {
 
 if (sum !== 6) {
   throw new Error("Expected sum is not equal to 6");
+}
+
+const result = map([1, 2, 3], (value) => {
+  return value * 2;
+});
+
+if (result[0] !== 2) {
+  throw new Error(`Expected to find 2, but found ${result[0]}`);
+}
+if (result[1] !== 4) {
+  throw new Error(`Expected to find 4, but found ${result[1]}`);
+}
+if (result[2] !== 6) {
+  throw new Error(`Expected to find 6, but found ${result[2]}`);
 }
