62423995adc1da75facf8e8d20a0131fef8dd69e
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-05-11 07:10:26 -0400

    Testing the search option

--- 32_testing_movies/test/autocomplete.test.js
+++ 32_testing_movies/test/autocomplete.test.js
@@ -1,5 +1,15 @@
 it("Shows an autocomplete!", () => {
   createAutoComplete({
     root: document.querySelector("#target"),
+    fetchData() {
+      return [
+        { Title: "Avengers" },
+        { Title: "Not Avengers" },
+        { Title: "Other movie" },
+      ];
+    },
+    renderOption(movie) {
+      return movie.Title;
+    },
   });
 });
