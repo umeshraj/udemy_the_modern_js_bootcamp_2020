6e86f9d30e3d0a13fe6dc4f1d8882ca64bb7521b
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-05-11 07:23:56 -0400

    Manually dispatch an input event

--- 32_testing_movies/test/autocomplete.test.js
+++ 32_testing_movies/test/autocomplete.test.js
@@ -1,4 +1,5 @@
-it("Shows an autocomplete!", () => {
+beforeEach(() => {
+  document.querySelector("#target").innerHTML = "";
   createAutoComplete({
     root: document.querySelector("#target"),
     fetchData() {
@@ -12,8 +13,19 @@ it("Shows an autocomplete!", () => {
       return movie.Title;
     },
   });
+});
 
+it("Dropdown starts closed!", () => {
   const dropdown = document.querySelector(".dropdown");
 
   expect(dropdown.className).not.to.include("is-active");
+});
+
+it("After searching, dropdown opens up", () => {
+  const input = document.querySelector("#target");
+  input.value = "avengers";
+  input.dispatchEvent(new Event("input"));
+
+  const dropdown = document.querySelector(".dropdown");
+  expect(dropdown.className).to.include("is-active");
 });
