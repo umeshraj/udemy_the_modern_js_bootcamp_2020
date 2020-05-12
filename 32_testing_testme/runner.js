d83b9fc4c2afda950086bd1bd818844fb07f3677
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-05-12 07:14:54 -0400

    Implement our own beforeEach

--- 32_testing_testme/runner.js
+++ 32_testing_testme/runner.js
@@ -8,6 +8,15 @@ class Runner {
 
   async runTests() {
     for (let file of this.testFiles) {
+      const beforeEaches = [];
+      global.beforeEach = (fn) => {
+        beforeEaches.push(fn);
+      };
+
+      global.it = (desc, fn) => {
+        beforeEaches.forEach((func) => func());
+        fn();
+      };
       require(file.name);
     }
   }
--- 32_testing_testme/sampleproject/test/forEach.test.js
+++ 32_testing_testme/sampleproject/test/forEach.test.js
@@ -1,11 +1,25 @@
 const assert = require("assert");
 const { forEach } = require("../index");
 
+let numbers;
+beforeEach(() => {
+  numbers = [1, 2, 3];
+});
+
 it("Should sum an array", () => {
-  const numbers = [1, 2, 3];
+  let total = 0;
   forEach(numbers, (value) => {
     total += value;
   });
 
   assert.strictEqual(total, 6);
+  // wreck numbers array for checking it
+  numbers.push(3);
+  numbers.push(3);
+  numbers.push(3);
+  numbers.push(3);
+});
+
+it("beforeEach is run each time", () => {
+  assert.strictEqual(numbers.length, 3);
 });
