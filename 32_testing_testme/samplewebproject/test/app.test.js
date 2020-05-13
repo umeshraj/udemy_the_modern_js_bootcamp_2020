a03dc6616402436fd1ad90ea8272c18a1ae85a1e
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-05-13 07:44:51 -0400

    Adding test for invalid email

--- 32_testing_testme/samplewebproject/test/app.test.js
+++ 32_testing_testme/samplewebproject/test/app.test.js
@@ -15,5 +15,19 @@ it("shows a success messages with a valid email", async () => {
     .dispatchEvent(new dom.window.Event("submit"));
 
   const h1 = dom.window.document.querySelector("h1");
-  console.log("Contents of h1", h1.innerHTML);
+
+  assert.strictEqual(h1.innerHTML, "Looks good");
+});
+
+it("shows a failed messages with an invalid email", async () => {
+  const dom = await render("index.html");
+  const input = dom.window.document.querySelector("input");
+  input.value = "asasafam";
+  dom.window.document
+    .querySelector("form")
+    .dispatchEvent(new dom.window.Event("submit"));
+
+  const h1 = dom.window.document.querySelector("h1");
+
+  assert.strictEqual(h1.innerHTML, "Invalid email");
 });
